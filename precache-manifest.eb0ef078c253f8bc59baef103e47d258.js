ef4ef4be96d288ffdfcb5aeaae7f1c0f8987f0a3
Brad Weldy <bradmin@Brads-MacBook-Pro.local>
2019-08-20 21:05:07 -0700

    Updates

---
+++ precache-manifest.eb0ef078c253f8bc59baef103e47d258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f328d1f3ea5a1f708c6ea734eade783e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cfbad3c177b119ec6b2c",
+    "url": "/monsters-rolodex/static/css/main.ef424635.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "cfbad3c177b119ec6b2c",
+    "url": "/monsters-rolodex/static/js/main.81d17cb3.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
